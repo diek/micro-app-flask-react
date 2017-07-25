66926a258cd70878f814dc20a515988076da61ce
derrick kearney <diek@bellaliant.net>
2017-07-24 21:09:19 -0300

    Add Flask Blueprints and create api
    - Adding the api added views.py and models.py add better structure

--- project/__init__.py
+++ project/__init__.py
@@ -1,7 +1,6 @@
 # project/__init__.py
 
 import os
-import datetime
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
@@ -17,23 +16,18 @@ app.config.from_object(app_settings)
 db = SQLAlchemy(app)
 
 
-class User(db.Model):
-    __tablename__ = "users"
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(128), nullable=False)
-    email = db.Column(db.String(128), nullable=False)
-    active = db.Column(db.Boolean(), default=False, nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False)
+def create_app():
+    app = Flask(__name__)
 
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
-        self.created_at = datetime.datetime.utcnow()
+    # set config
+    app_settings = os.getenv('APP_SETTINGS')  # Set in docker-compose.yml
+    app.config.from_object(app_settings)
 
+    # set up extensions => Not clear on what this does
+    db.init_app(app)
 
-@app.route('/ping', methods=['GET'])
-def ping_pong():
-    return jsonify({
-        'status': 'success',
-        'message': 'pong!'
-    })
+    # register blueprints
+    from project.api.views import users_blueprint
+    app.register_blueprint(users_blueprint)
+
+    return app
