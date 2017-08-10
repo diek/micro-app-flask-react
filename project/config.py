90727a675ad476eb2ff814267d6a96f02dada431
derrick kearney <diek@bellaliant.net>
2017-08-10 00:36:26 -0300

    Created project structure
    - project config and init
    - docker config
    - Postgres settings

---
+++ project/config.py
@@ -0,0 +1,28 @@
+# project/config.py
+import os
+
+
+class BaseConfig:
+    """docstring for Base Configuration"""
+    DEBUG = False
+    TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class DevelopmentConfig(BaseConfig):
+    """docstring for Development Configuration"""
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+
+
+class TestingConfig(BaseConfig):
+    """docstring for Testing Config"""
+    DEBUG = True
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
+
+
+class ProductionConfig(BaseConfig):
+    """docstring for Production Config"""
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
